23101bd6079f55bbfd1b1cbcea5c169dd0556e2b
dapc11 <pitsson13@gmail.com>
2013-09-06 13:45:14 +0200

    Added argument functionality

--- log_sum.sh
+++ log_sum.sh
@@ -1,23 +1,78 @@
 #!/bin/bash
 
-while test $# -gt 0; do
-	case "$1" in 
-		--help)
-			echo " "
-			echo 'Usage: log_sum(.sh|.py) [-n N] [-h H|-d D][-c|-2|
--r|-F|-t|-f]<filename>'                        
-                        echo " "
-                        echo "options:"
-                        echo "-n			Limit the number of results to N"
-                        echo "-h			Limit the query to the last number of hours (< 24)"
-			echo "-d			Limit the query to the last number of days (counting from midnight)"
-			echo "-c			Which IP address makes the most number of connection attempts?"
-			echo "-2			Which address makes the most number of successful attempts?"
-			echo "-r			What are the most common results codes and where do they come from?"
-			echo "-F			What are the most common result codes that indicate failure (no auth, not found etc) and where do they come from?"
-			echo "-t			Which IP number get the most bytes sent to them?"
-			echo "-f			Which IP number sends the most bytes to the server"
-                        exit 0
-			;;
+NROFRESULTS=
+NROFHOURS=
+NROFDAYS=
+MOSTCONNECTATTEMPTS=
+MOSTSUCCESSFULATTEMPTS=
+FUNCTION=
+
+while getopts “h:n:d:c2rFtf” OPTION
+do
+	case $OPTION in
+	n)
+		NROFRESULTS=$OPTARG
+	;;
+	h)
+		if [[ "$@" != *-d* ]]
+		then
+			NROFHOURS=$OPTARG
+			echo $NROFHOURS
+		else
+			exit 1;
+		fi	
+	;;
+	d)
+		if [[ "$@" != *-h* ]]
+		then
+			NROFDAYS=$OPTARG
+		else
+			exit 1;
+		fi	
+	;;
+	c)
+		if [[ $@ != *'-2'* && $@ != *'-r'* && $@ != *'-F'* && $@ != *'-t'* && $@ != *'-f'* ]]; then
+			FUNCTION='c' >&2
+		else
+			exit 1;
+		fi
+	;;
+	2)
+		if [[ $@ != *'-c'* && $@ != *'-r'* && $@ != *'-F'* && $@ != *'-t'* && $@ != *'-f'* ]]; then
+			FUNCTION='2' >&2
+		else
+			exit 1;
+		fi
+	;;
+	r)
+		if [[ $@ != *'-2'* && $@ != *'-c'* && $@ != *'-F'* && $@ != *'-t'* && $@ != *'-f'* ]]; then
+			FUNCTION='r' >&2
+		else
+			exit 1;
+		fi
+	;;
+	F)
+		if [[ $@ != *'-2'* && $@ != *'-r'* && $@ != *'-c'* && $@ != *'-t'* && $@ != *'-f'* ]]; then
+			FUNCTION='F' >&2
+		else
+			exit 1;
+		fi
+	;;
+	t)
+		if [[ $@ != *'-2'* && $@ != *'-r'* && $@ != *'-F'* && $@ != *'-c'* && $@ != *'-f'* ]]; then
+			FUNCTION='t' >&2
+		else
+			exit 1;
+		fi
+	;;
+	f)
+		if [[ $@ != *'-2'* && $@ != *'-r'* && $@ != *'-F'* && $@ != *'-t'* && $@ != *'-c'* ]]; then
+			FUNCTION='f' >&2
+		else
+			exit 1;
+		fi
+	;;
 	esac
 done
+
+echo $FUNCTION
